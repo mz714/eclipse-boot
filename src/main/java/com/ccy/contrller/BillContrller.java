700cca5fa6bbb9c517e96887f6788b75a66b5724
mz <2305397985@qq.com>
2018-09-14 20:19:49 +0800

    aa

---
+++ src/main/java/com/ccy/contrller/BillContrller.java
@@ -0,0 +1,27 @@
+package com.ccy.contrller;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ccy.po.Bill;
+import com.ccy.service.BillService;
+//aaaa
+@Controller
+@RequestMapping("/list")
+public class BillContrller {
+	@Resource
+	private BillService billService;
+	
+	@RequestMapping("/bill")
+	public String list(Model model) {
+	 List<Bill> bills = billService.bills();
+	 model.addAttribute("bill", bills);
+	 return "list";
+	}
+
+}
